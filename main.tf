c57c3c62fb7f226b57b02754e9ab643d7c6131f8
psurya5190 <111116944+psurya5190@users.noreply.github.com>
2022-10-05 19:42:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ variable "cidr_block" {}
 variable "aws_region" {
   default = "ap-south-1"
 }
-variable "aws_secret_key" {}
-variable "aws_access_key" {}
+#variable "aws_secret_key" {}
+#variable "aws_access_key" {}
 
 provider "aws" {
   region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  #access_key = var.aws_access_key
+  #secret_key = var.aws_secret_key
   version    = "= 4.21.0"
 }
 
